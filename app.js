4370d7309d9f884b87cf55b8fecbe144e57d7695
Aliaksei Siankevich <andfly@tut.by>
2020-05-27 10:57:03 +0300

    calculator-3: create a left operand

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
 var leftOperand;
 var operator;
 var rightOperand;
+
+for(leftOperand = prompt("Левый операнд"); isNaN(leftOperand);leftOperand = +leftOperand){
+    alert("Введите число!");
+    leftOperand = prompt("Левый операнд");
+}
